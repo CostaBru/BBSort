85d46c9e1e8330e69941207270cd92cd8c29a9c2
Konstantin Briukhnov (Costa Bru) <kooltew@gmail.com>
2021-04-20 10:33:49 -0700

    Count dict moved out. Minor changes.

--- python3/bb_sort.py
+++ python3/bb_sort.py
@@ -28,9 +28,14 @@ def bb_sort(array, iterCounter):
      :param iterCounter: iteration counter
      :type iterCounter: array
     """
+    countMap = defaultdict(int)
+
+    for item in array: countMap[item] += 1
+
+    iterCounter[0] += len(countMap)
 
     i = 0
-    for item in bb_sort_core_to_iter(array, len(array),  iterCounter):
+    for item in bb_sort_core_to_iter(countMap.keys(),  iterCounter, countMap):
         array[i] = item
         i += 1
 
@@ -48,96 +53,98 @@ def bb_sort_to_stream(array, stream, iterCounter):
     :type iterCounter: array
     """
 
-    bb_sort_core_to_stream(array, len(array), stream, iterCounter)
+    countMap = defaultdict(int)
+
+    for item in array: countMap[item] += 1
+
+    iterCounter[0] += len(countMap)
+
+    bb_sort_core_to_stream(countMap.keys(), stream, iterCounter, countMap)
 
 
-def getBuckets(enumerable, count, countMap, iterCounter):
+def get_buckets(iterable, iterCounter):
 
-    def getLog(x):
+    def get_log(x):
         ax = abs(x)
         if abs(x) < 2: return x
         return math.log2(x) if x > 0 else -math.log2(ax)
 
-    def GetLinearTransformParams(x1, x2, y1, y2):
+    def get_linear_transform_params(x1, x2, y1, y2):
         dx = x1 - x2
         if dx == 0: return 0, 0
         a = (y1 - y2) / dx
         b = y1 - (a * x1)
         return a, b
 
-    min_element, max_element, size = sys.maxsize, -sys.maxsize, count
+    minElement, maxElement, count = min(iterable), max(iterable), len(iterable)
 
-    buckets = [None] * (size + 1)
+    iterCounter[0] += count * 2
 
-    for item in enumerable: countMap[item] += 1; min_element = min(min_element, item); max_element = max(max_element, item)
+    size = (count // 2 + 1)
 
-    iterCounter[0] += size * 3
+    buckets = [None] * size
 
-    a, b = GetLinearTransformParams(getLog(min_element), getLog(max_element), 0, size)
+    a, b = get_linear_transform_params(get_log(minElement), get_log(maxElement), 0, size)
 
-    for key in countMap.keys():
+    for item in iterable:
         # ApplyLinearTransform
-        index = int((a *  getLog(key)) + b)
+        index = min(int((a *  get_log(item)) + b), size - 1)
         bucket = buckets[index]
-        if bucket: bucket.append(key)
-        else:      buckets[index] = [key]
+        if bucket: bucket.append(item)
+        else:      buckets[index] = [item]
 
-    iterCounter[0] += len(countMap)
+    iterCounter[0] += len(iterable)
 
     return buckets
 
 
-def bb_sort_core_to_stream(enumerable, count, output, iterCounter):
+def bb_sort_core_to_stream(enumerable, output, iterCounter, countMap):
 
-    def fillStream(val, output, countMap, iterCounter):
+    def fill_stream(val, output, countMap, iterCounter):
         valCount = countMap[val]
         iterCounter[0] += valCount
         for j in range(valCount):
             output.append(val)
 
-    countMap = defaultdict(int)
-
-    buckets = getBuckets(enumerable, count, countMap, iterCounter)
+    buckets = get_buckets(enumerable, iterCounter)
 
     for bucket in buckets:
         if bucket:
             bucketCount = len(bucket)
             if bucketCount == 1:
-                fillStream(bucket[0], output, countMap, iterCounter)
+                fill_stream(bucket[0], output, countMap, iterCounter)
             elif bucketCount == 2:
                 b1, b2 = bucket[0], bucket[1]
                 if b1 > b2: b1, b2 = b2, b1
-                fillStream(b1, output, countMap, iterCounter)
-                fillStream(b2, output, countMap, iterCounter)
+                fill_stream(b1, output, countMap, iterCounter)
+                fill_stream(b2, output, countMap, iterCounter)
             else:
-                bb_sort_core_to_stream(bucket, bucketCount, output, iterCounter)
+                bb_sort_core_to_stream(bucket, output, iterCounter, countMap)
 
-def bb_sort_core_to_iter(enumerable, count, iterCounter):
+def bb_sort_core_to_iter(enumerable, iterCounter, countMap):
 
-    def iterArr(val, countMap, iterCounter):
+    def iter_arr(val, countMap, iterCounter):
         valCount = countMap[val]
         iterCounter[0] += valCount
         for j in range(valCount):
             yield val
 
-    countMap = defaultdict(int)
-
-    buckets = getBuckets(enumerable, count, countMap, iterCounter)
+    buckets = get_buckets(enumerable, iterCounter)
 
     for bucket in buckets:
         if bucket:
             bucketCount = len(bucket)
             if bucketCount == 1:
-                for item in iterArr(bucket[0], countMap, iterCounter):
+                for item in iter_arr(bucket[0], countMap, iterCounter):
                     yield item      
             elif bucketCount == 2:            
                 b1, b2 = bucket[0], bucket[1]
                 if b1 > b2:
                     b1, b2 = b2, b1               
-                for item in iterArr(b1, countMap, iterCounter):
+                for item in iter_arr(b1, countMap, iterCounter):
                     yield item 
-                for item in iterArr(b2, countMap, iterCounter):
+                for item in iter_arr(b2, countMap, iterCounter):
                     yield item     
             else:
-                for item in bb_sort_core_to_iter(bucket, bucketCount, iterCounter):
+                for item in bb_sort_core_to_iter(bucket, iterCounter, countMap):
                     yield item
